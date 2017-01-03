4dac5ebf3c78616d4d03186a07cff9258eb42dff
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-01-03 14:32:00 +0800

    Create transport.js

---
+++ devicesvc/transport.js
@@ -0,0 +1,20 @@
+
+import Router from 'koa-router'
+import Endpoint from './endpoint.js'
+import Service from './service.js'
+import Device from './model.js'
+
+const route = new Router()
+
+route.use(async(ctx, next) => {
+  // Manually inject the service in the context
+  ctx.service = Service({
+    db: Device
+  })
+  await next()
+})
+
+route.get('/devices', Endpoint.getDevices)
+route.get('/devices/:id', Endpoint.getDevice)
+
+export default route
