207b04020d53193dc3e81a6b633c8744e888a808
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-01-03 14:29:45 +0800

    Update endpoint.js

--- devicesvc/endpoint.js
+++ devicesvc/endpoint.js
@@ -1,1 +1,35 @@
 // endpoint.js
+
+// GET /devices
+// Description: Get a list of devices
+const getDevices = async(ctx, next) => {
+  const request = getDevicesRequest(ctx.request.body)
+  
+  if (request.select) {
+    // select only devices that are tied to the user's id
+  }
+  const devices = ctx.service.getDevices(request)
+  const response = getDevicesResponse(devices)
+  // do something with the response
+  this.status = 200
+  this.body = response
+}
+
+// GET /devices/:id
+// Description: Get a single device by the device id
+const getDevice = async(ctx, next) => {
+ // Get a single device by Id 
+}
+
+
+// Request/Response
+const getDevicesRequest = (req) => {
+  return req 
+}
+const getDevicesResponse = (res) => {
+  return res
+}
+
+export default {
+ getDevices 
+}
