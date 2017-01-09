18e826f4fd1004edfc8df32b1e8a189bbf8cbb10
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-01-09 18:45:30 +0800

    Update introspect-post.js

--- oauthsvc/endpoints/introspect-post.js
+++ oauthsvc/endpoints/introspect-post.js
@@ -4,63 +4,43 @@
 // Reference: http://connect2id.com/products/server/docs/api/token-introspection
 import base64 from '../../modules/base64.js'
 
-
-// @res is the response from the service
-// HTTP/1.1 200 OK
-// Content-Type: application/json
-// Cache-Control: no-store
-// Pragma: no-cache
-
-// HTTP/1.1 200 OK
-// Content-Type: application/json;charset=UTF-8
-// {
-//   "active"     : true,
-//   "scope"      : "https://example.com/accounts https://example.com/groups",
-//   "client_id"  : "izad7cqy34bg4",
-// username
-//   "token_type" : "Bearer",
-//   "exp"        : 1448367412,
-// iat
-// nbf
-//   "sub"        : "izad7cqy34bg4",
-// aud
-//   "iss"        : "https://c2id.com"
-// jti
-// dat
-// }
-function response (res) {
-  return {
-    active: res.active
-  }
-}
-
+// Standardize the errors: either invalid (wrong, not supported, etc), 
+// missing (required, but not provided), or forbidden (no permission)
+const ErrorInvalidContentType = new Error('Invalid Content Type: Content-Type must be application/json')
+const ErrorBasicAuthorizationMissing = new Error('Invalid Request: Basic authorization header is required')
+const ErrorForbiddenAccess = new Error('Forbidden Access: Client does not have permission to access this service')
 
 const introspect = async (ctx, next) => {
+  // Business-rule-validation
   if (!ctx.is('application/json')) {
-    ctx.throw('Invalid Content Type: Content-Type must be application/json', 400)
+    ctx.throw('Bad Request', 400, {
+      description: ErrorInvalidContentType.message
+    })
   }
+  const authorizationHeader = ctx.headers.authorization
+  const [ authType, authToken ] = authorizationHeader.split(' ')
+  if (authType !== 'Basic') {
+    ctx.throw('Bad Request', 400, {
+      description: ErrorBasicAuthorizationMissing.message
+    })
+  }
+  const authTokenValidated = base64.decode(authToken)
+  const [ clientId, clientSecret ] = authTokenValidated.split(':')
+  if (!clientId || !clientSecret) {
+    // throw error
+    ctx.throw('Forbidden Access', 403, {
+      description: ErrorForbiddenAccess.message
+    })
+  }
+  // Fire external service
+  // const client = await this.service.getClient({ clientId, clientSecret })
+  // if (!client) {
+  //   throw new Error('Forbidden Access: Client does not have permission to access this service') 
+  // }
   try {
-    const auth = ctx.headers.authorization
-    const [ authType, authToken ] = auth.split(' ')
-    if (authType !== 'Basic') {
-      // throw error
-      throw new Error('Invalid Request: Basic authorization header is required')
-    }
-    const authTokenValidated = base64.decode(authToken)
-    const [ clientId, clientSecret ] = authTokenValidated.split(':')
-    if (!clientId || !clientSecret) {
-      // throw error
-      throw new Error('Forbidden Access: Client does not have permission to access this service')
-    }
-    // Fire external service
-    // const client = await this.service.getClient({ clientId, clientSecret })
-    // if (!client) {
-    //   throw new Error('Forbidden Access: Client does not have permission to access this service') 
-    // }
-
     const request = {
-      token_type_hint: ctx.request.body.token_type_hint,
-      token: ctx.request.body.token
+      token: ctx.request.body.token,
+      token_type_hint: ctx.request.body.token_type_hint
     }
 
     const output = await ctx.service.postIntrospect(request)
@@ -91,7 +71,7 @@ const introspect = async (ctx, next) => {
   } catch (err) {
     // ctx.status = 500
     // this.error()
-    ctx.status = 400 // 400, 401, 402, 500
+    ctx.status = 500 // 400, 401, 402, 500
     ctx.body = {
       error: err.name, // TokenExpiredError 
       error_description: err.message
