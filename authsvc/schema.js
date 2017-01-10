33eecfbf04ba87a1994af4d85e8f78c7edb2b342
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-01-10 11:04:13 +0800

    Update schema.js

--- authsvc/schema.js
+++ authsvc/schema.js
@@ -1,1 +1,17 @@
-// Added schema files
+import Ajv from 'ajv'
+const ajv = Ajv({
+  allErrors: true,
+  schemas: [
+    require('./schema/login-request.json'),
+    require('./schema/login-response.json'),
+    require('./schema/register-request.json'),
+    require('./schema/register-response.json')
+  ]
+})
+
+export default {
+  loginRequest: ajv.getSchema('http://localhost:3000/schemas/login-request.json#'),
+  loginResponse: ajv.getSchema('http://localhost:3000/schemas/login-response.json#'),
+  registerRequest: ajv.getSchema('http://localhost:3000/schemas/register-request.json#'),
+  registerResponse: ajv.getSchema('http://localhost:3000/schemas/register-response.json#'),
+}
