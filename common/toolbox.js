3a899addda7a80a522ddc770a4f75c65b6b8c525
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-01-11 18:38:13 +0800

    Create toolbox.js

---
+++ common/toolbox.js
@@ -0,0 +1,33 @@
+const parser = (ajv) => {
+  const request = (schema) => {
+    return (req) => {
+      const validator = ajv.getSchema(schema)
+      const isValid = validator(req)
+      if (!isValid) {
+        const error = new Error('Invalid Request')
+        error.description = validator.errors[0].message
+        throw error
+      }
+      return req
+    }
+  }
+  
+  const response = (schema) => {
+    return (res) => {
+      const validator = ajv.getSchema(schema)
+      const isValid = validator(res)
+      if (!isValid) {
+        const error = new Error('Invalid Response')
+        error.description = validator.errors[0].message
+        throw error
+      }
+      return res
+    }
+  }
+  return  {
+    request,
+    response
+  }
+}
+
+export default parser
