78b2baaa766cf1418daae4a4a4fb8e54249245bc
alextanhongpin <alextan220990@gmail.com>
2017-02-09 12:50:30 +0800

    updated client endpoint

--- clientsvc/endpoint.js
+++ clientsvc/endpoint.js
@@ -70,7 +70,7 @@ class Endpoint {
   }
   // GET /clients
   // Description: Returns a list of clients
-  async getClients (ctx, next) {
+  async all (ctx, next) {
     // NOTE:
     // try-catch is redundant here since the global error
     // handler will capture the error
@@ -78,7 +78,7 @@ class Endpoint {
     // errors (like redirection)
     // try {
     const request = getClientsRequest(ctx.query)
-    const clients = await ctx.service.getClients(request)
+    const clients = await ctx.service.all(request)
     const response = getClientsResponse(clients)
     ctx.status = 200
     ctx.body = response
@@ -86,12 +86,12 @@ class Endpoint {
 
   // GET /clients/:id
   // Description: Return a client by id
-  async getClient (ctx, next) {
+  async one (ctx, next) {
 
     const request = schema.getClientRequest({
       _id: ctx.params.id
     })
-    const client = await ctx.service.getClientById(request)
+    const client = await ctx.service.one(request)
     const response = client
     // this.set('Cache-Control', 'no-cache')
     // this.set('Pragma', 'no-cache')
@@ -101,7 +101,7 @@ class Endpoint {
   }
   // POST /clients
   // Description: Create a new client
-  async postClient (ctx, next) {
+  async create (ctx, next) {
     try {
       const request =  ctx.schema.postClientRequest({
         contacts: ctx.request.body.contacts,
@@ -112,7 +112,7 @@ class Endpoint {
         policy_uri: ctx.request.body.policyURI,
         redirect_uris: ctx.request.body.redirectURIs
       })
-      const client = await ctx.service.postClient(request)
+      const client = await ctx.service.create(request)
       // NOTE: the client object returned is not pure JSON, causing
       // error for AJV to parse
       const response = ctx.schema.postClientResponse(JSON.parse(JSON.stringify(client)))
@@ -136,7 +136,7 @@ class Endpoint {
     }
   }
 
-  async updateClient (ctx, next) {
+  async update (ctx, next) {
     // Add a permission setting to allow only authorized user to update
     try {
       const request = ctx.schema.updateClientRequest({
@@ -149,9 +149,9 @@ class Endpoint {
         redirect_uris: ctx.request.body.redirectURIs
       })
 
-      const client = await ctx.service.updateClient(ctx.params.id, request)
+      const client = await ctx.service.update(ctx.params.id, request)
       const response = client
-      
+
       ctx.status = 200
       ctx.body = client
     } catch (err) {
